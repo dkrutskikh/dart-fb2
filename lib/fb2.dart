4806777f9ffc49f192acb7fcf961421526371307
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2020-10-18 23:19:32 +0300

    read genre

--- lib/fb2.dart
+++ lib/fb2.dart
@@ -1,1 +1,2 @@
+export 'src/entities/fb2_genre.dart';
 export 'src/fb2_reader.dart';
--- lib/src/entities/fb2_title.dart
+++ lib/src/entities/fb2_title.dart
@@ -1,11 +1,14 @@
 import 'package:meta/meta.dart';
 
 import 'fb2_creator.dart';
+import 'fb2_genre.dart';
 
 @immutable
 class Fb2Title {
   final String title;
   final Iterable<Fb2Creator> authors;
+  final Iterable<Fb2Genre> genres;
 
-  const Fb2Title({@required this.title, @required this.authors});
+  const Fb2Title(
+      {@required this.title, @required this.authors, @required this.genres});
 }
